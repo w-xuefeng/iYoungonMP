90ecaa55a176f2a3bb68a7b4606fb0fa4a51dd5f
XueFLY <xuefeng@kcalb.wang>
2020-07-04 01:00:11 +0800

    feat: finished myresume page

--- src/models/Resume.ts
+++ src/models/Resume.ts
@@ -1,29 +1,59 @@
 export class Resume {
-  sex: string;
-  name: string;
-  work: { workurl: string; workdesc: string; }[];
-  email: string;
-  phone: string;
-  skill: string[];
-  stuid: string | number;
-  demand: string;
-  nation: string;
-  college: string;
-  birthday: string;
-  softlang: string[];
-  department: string;
-  majorclass: string;
-  nativeplace: string;
-  selfcomment: string[];
+  sex: string = '';
+  name: string = '';
+  work: { workurl: string; workdesc: string; }[] = [];
+  email: string = '';
+  phone: string = '';
+  skill: string[] = [];
+  stuid: string | number = '';
+  demand: string = '';
+  nation: string = '';
+  college: string = '';
+  birthday: string = '';
+  softlang: string[] = []
+  department: string = '';
+  majorclass: string = '';
+  nativeplace: string = '';
+  selfcomment: string[] = []
   activityapply: {
     actdesc: string;
     actname: string;
     actrole: string;
     acttime: string;
-  }[];
-  computermaster: 0 | 1 | 2;
-  departmentreason: string;
-  constructor() {}
+  }[] = [];
+  computermaster: 0 | 1 | 2 = 0;
+  departmentreason: string = '';
+
+  constructor(option?: Partial<Resume>) {
+    if (option) {
+      const keys = [
+        'sex',
+        'name',
+        'work',
+        'email',
+        'phone',
+        'skill',
+        'stuid',
+        'demand',
+        'nation',
+        'college',
+        'birthday',
+        'softlang',
+        'department',
+        'majorclass',
+        'nativeplace',
+        'selfcomment',
+        'activityapply',
+        'computermaster',
+        'departmentreason'
+      ];
+      Object.keys(option).forEach(key => {
+        if (keys.includes(key)) {
+          this[key] = option[key];
+        }
+      })
+    }
+  }
 }
 
 export default Resume
--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,10 +1,9 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import { Resume, User } from '@/models'
-// import {
-// } from 'taro-ui'
+import { AtCard, AtTag, AtProgress } from 'taro-ui'
 import { LocalData, LDKey, handelUserInfo } from '@/utils/index'
-import { modifyUserPhoto, getUserInfo } from '@/api'
+import { modifyUserPhoto, getUserInfo, modifyUserInfo } from '@/api'
 import './index.scss'
 
 
@@ -13,10 +12,92 @@ import './index.scss'
  */
 const defaultPhoto = 'https://pub.wangxuefeng.com.cn/asset/defaultHead/1c.jpg'
 
+
+const baseInfo = [
+  {
+    key: 'name',
+    text: '姓名'
+  },
+  {
+    key: 'sex',
+    text: '性别'
+  },
+  {
+    key: 'stuid',
+    text: '学号'
+  },
+  {
+    key: 'birthday',
+    text: '出生日期'
+  },
+  {
+    key: 'nation',
+    text: '民族'
+  },
+  {
+    key: 'nativeplace',
+    text: '籍贯'
+  },
+  {
+    key: 'email',
+    text: '邮箱'
+  },
+  {
+    key: 'phone',
+    text: '电话'
+  },
+  {
+    key: 'college',
+    text: '学院'
+  },
+  {
+    key: 'majorclass',
+    text: '班级'
+  }
+]
+const otherInfo = [
+  {
+    key: 'skill',
+    text: '个人特长'
+  },
+  {
+    key: 'computermaster',
+    text: '计算机掌握情况'
+  },
+  {
+    key: 'softlang',
+    text: '了解的计算机软件及语言'
+  },
+  {
+    key: 'selfcomment',
+    text: '三个词概括自己'
+  },
+  {
+    key: 'department',
+    text: '意向部门'
+  },
+  {
+    key: 'departmentreason',
+    text: '为什么选择这个部门'
+  },
+  {
+    key: 'demand',
+    text: '你觉得你在这里能够得到什么'
+  },
+  {
+    key: 'work',
+    text: '作品'
+  },
+  {
+    key: 'activityapply',
+    text: '活动经历'
+  }
+]
+
 type MyResumeState = {
-  stuid: string | number;
+  photo?: string | null;
+  currentUser: User;
   resume: Resume;
-  photo: string | null | undefined;
 }
 
 export default class MyResume extends Component<{}, MyResumeState> {
@@ -41,9 +122,9 @@ export default class MyResume extends Component<{}, MyResumeState> {
     super(...arguments)
     const user: User = LocalData.getItem(LDKey.USER)
     this.state = {
-      stuid: user.stuid,
       photo: user.photo,
-      resume: JSON.parse(user.interviewform || 'null') || new Resume()
+      currentUser: user,
+      resume: JSON.parse(user.interviewform || 'null') || new Resume({ ...user, sex: user.sexName })
     }
   }
 
@@ -74,8 +155,7 @@ export default class MyResume extends Component<{}, MyResumeState> {
   updateUserInfoAndCheckOnline(user: User) {
     const curUser: User = handelUserInfo(user)
     this.setState({
-      stuid: curUser.stuid,
-      photo: curUser.photo,
+      currentUser: curUser,
       resume: JSON.parse(curUser.interviewform || 'null'),
     })
     this.setOnline(Number(user.online) === 1)
@@ -102,7 +182,8 @@ export default class MyResume extends Component<{}, MyResumeState> {
       sizeType: ['compressed'],
       sourceType: ['album', 'camera'],
       success: res => {
-        const { stuid } = this.state
+        const { currentUser } = this.state
+        const { stuid } = currentUser
         const tempFilePaths = res.tempFilePaths
         const photoBase64 = Taro
           .getFileSystemManager()
@@ -124,20 +205,161 @@ export default class MyResume extends Component<{}, MyResumeState> {
     })
   }
 
+  modifyMyResume() {
+    const { currentUser, resume } = this.state
+    const { stuid } = currentUser
+    const info = 'interviewform'
+    const { str: value, obj } = this.genInterviewform(currentUser, resume)
+    modifyUserInfo({ stuid, info, value }).then(data => {
+      if (data.status) {
+        this.refreshPage().then(() => {
+          this.setLocalData({ interviewform: value })
+          currentUser.interviewform = value
+          this.setState({
+            currentUser,
+            resume: obj,
+          })
+          Taro.showToast({ title: `简历保存成功`, icon: 'success' })
+        })
+      }
+    })
+  }
+
+  genInterviewform(user: User, resume: Resume) {
+    Object.keys(new Resume()).map(key => {
+      if (!resume[key] && user[key]) {
+        resume[key] = user[key]
+      }
+    })
+    if (user.sexName) {
+      resume.sex = user.sexName
+    }
+    return {
+      obj: resume,
+      str: JSON.stringify(resume),
+    }
+  }
+
+  genContent(resume: Resume, key: string) {
+    if (['skill', 'softlang', 'selfcomment'].includes(key)) {
+      return (
+        <View style='display: flex;flex-wrap: wrap;'>
+          {
+            resume[key] && resume[key].length
+              ? resume[key].map((k, i) => (
+                  <AtTag
+                    className='resume-mr-10'
+                    name={`tag-${key}-${i}`}
+                    key={`tag-${key}-${i}`}
+                    size='small'
+                    active
+                  >
+                    {k}
+                  </AtTag>
+                )) : '暂未填写'
+          }
+        </View>
+      )
+    }
+    if (key === 'computermaster') {
+      const value = [10, 66, 99]
+      return (
+        <AtProgress percent={value[resume[key]]} color='#007ACC' />
+      )
+    }
+    if (key === 'department' && resume[key]) {
+      return (
+        <AtTag
+          name={`tag-${key}`}
+          size='small'
+          active
+        >
+          {resume[key]}
+        </AtTag>
+      )
+    }
+    if (key === 'work') {
+      return (
+        <View style='display: flex;flex-direction: column;'>
+          {
+            resume[key] && resume[key].length
+              ? resume[key].map((k, i) => (
+                <View key={`work-${i}`} style='display: flex;flex-direction: column;margin: 5px 0'>
+                  <Text>作品链接: {k.workurl || '暂未填写'}</Text>
+                  <Text>作品描述: {k.workdesc || '暂未填写'}</Text>
+                </View>
+              )) : '暂未填写'
+          }
+        </View>
+      )
+    }
+    if (key === 'activityapply') {
+      return (
+        <View style='display: flex;flex-direction: column;'>
+          {
+            resume[key] && resume[key].length
+              ? resume[key].map((k, i) => (
+                <View key={`work-${i}`} style='display: flex;flex-direction: column;margin: 5px 0'>
+                  <Text>活动名称: {k.actname || '暂未填写'}</Text>
+                  <Text>活动时间: {k.acttime || '暂未填写'}</Text>
+                  <Text>担任的职责: {k.actrole || '暂未填写'}</Text>
+                  <Text>活动具体描述: {k.actdesc || '暂未填写'}</Text>
+                </View>
+              )) : '暂未填写'
+          }
+        </View>
+      )
+    }
+    return (
+      <Text>{resume[key] || '暂未填写'}</Text>
+    )
+  }
+
   render() {
-    const { resume, photo } = this.state
+    const { resume, currentUser, photo } = this.state
     return (
       <View className='page-resume yg-background'>
-        <View className='head-top'>
-          <View
-            className='photo'
-            style={{ backgroundImage: `url(${photo || defaultPhoto})` }}
-            onClick={this.modifyPhoto.bind(this)}
-          ></View>
-        </View>
-        <View className='info-bottom'>
-          {resume}
-        </View>
+        <AtCard
+          note='可前往 https://join.youngon.work 完善简历'
+          extra={currentUser.name}
+          title='我的简历'
+        >
+          <View className='head-top'>
+            <View
+              className='photo'
+              style={{ backgroundImage: `url(${photo || defaultPhoto})` }}
+              onClick={this.modifyPhoto.bind(this)}
+            ></View>
+            <View className='right-info'>
+              {
+                baseInfo.map(e => (
+                  <Text
+                    className='right-info-item'
+                    key={e.key}
+                  >
+                    <Text className='right-info-item-tips'>{e.text}</Text>：{
+                      resume[e.key] || currentUser[e.key] || '暂未填写'
+                    }
+                  </Text>
+                ))
+              }
+            </View>
+          </View>
+          <View className='info-bottom'>
+            {
+              otherInfo.map(e => (
+                <View className='flex-column' key={e.key}>
+                  <Text className='other-title'>{e.text}</Text>
+                  <View className='other-content'>
+                    {
+                      this.genContent(resume, e.key)
+                    }
+                  </View>
+                </View>
+              ))
+            }
+          </View>
+        </AtCard>
       </View>
     )
   }
